c91c1dd245538c9162b2790c0cecffdd0ed16396
Tyfu1113 <dottly113@gmail.com>
2024-05-19 07:51:47 +0800

    add comment

--- 8-3.cpp
+++ 8-3.cpp
@@ -19,6 +19,7 @@ private:
     double real;
     double imaginary;
 };
+//define i
 const Complex i=Complex(0, 1);
 const Complex operator +(const Complex& complex1, const Complex& complex2);
 const Complex operator -(const Complex& complex1, const Complex& complex2);
@@ -30,19 +31,28 @@ int main(){
     Complex complex1, complex2(2, 3);
     cout << "Enter an complex:" << endl;
     cin >> complex1;
+    //show two complex
     cout << "Complex 1 is " << complex1 << "\t" << "Complex2 is " << complex2 << endl;
+    //testing addition
     cout << "The addition of two complex is " << complex1+complex2 << endl;
+    //testing substraction
     cout << "The substraction of two complex is " << complex1-complex2 << endl;
+    //testing multiplication
     cout << "The multiplication of two complex is " << complex1*complex2 << endl;
+    //testing negative
     cout << "The negative of complex1 is " << -complex1 << endl;
+    //testing equal
     if(complex1==complex2){
         cout << complex1 << " is equals to " << complex2 << endl;
     }else{
         cout << complex1 << " is not equals to " << complex2 << endl;
     }
+    //testing constant 'i'
+    cout << 2*i << endl;
 
     return 0;
 }
+//three construstor
 Complex::Complex(){
     real=0;
     imaginary=0;
@@ -55,6 +65,7 @@ Complex::Complex(double realPart){
     real=realPart;
     imaginary=0;
 }
+//get, set method
 double Complex::getReal()const{
     return real;
 }
@@ -67,6 +78,7 @@ void Complex::setReal(double real){
 void Complex::setImag(double imag){
     imaginary=imag;
 }
+//overloading operator
 const Complex operator +(const Complex& complex1, const Complex& complex2){
     double real=complex1.getReal()+complex2.getReal();
     double imaginary=complex1.getImag()+complex2.getImag();
@@ -91,6 +103,9 @@ const Complex operator -(const Complex& complex){
 }
 ostream& operator <<(ostream& outputStream, const Complex& complex){
     outputStream << complex.real;
+    if(complex.real==0){ //if complex doesn't have real part
+        outputStream << complex.imaginary << "i";
+    }
     if(complex.imaginary<0){
         outputStream << complex.imaginary << "i";
     }else{
